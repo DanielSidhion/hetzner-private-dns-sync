12589082b44310042d395605ac8e2d5e3c9b5997
DS <commits@sidhion.com>
2024-10-03 14:32:47 -0700

    Add NixOS module to flake.

--- flake.nix
+++ flake.nix
@@ -63,5 +63,79 @@
           };
         };
       };
+
+      nixosModule = { config, lib, pkgs, ... }:
+        let cfg = config.services.hetzner-private-dns-sync;
+        in {
+          options.services.hetzner-private-dns-sync = {
+            enable = lib.mkEnableOption "Enables the hetzner-private-dns-sync service";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.x86_64-linux.default;
+              description = "The package to use for hetzner-private-dns-sync";
+            };
+
+            tsigKeyPath = lib.mkOption {
+              type = lib.types.path;
+              description = "The path to the TSIG key used to communicate with the DNS server.";
+            };
+
+            tsigKeyName = lib.mkOption {
+              type = lib.types.str;
+              description = "The name of the TSIG key used to communicate with the DNS server.";
+            };
+
+            serverAddress = lib.mkOption {
+              type = lib.types.str;
+              default = "udp://127.0.0.1:53";
+              description = "The address of the DNS server to update. Must be in the format (udp|tcp)://<ip>:<port>.";
+            };
+
+            environmentFilePath = lib.mkOption {
+              type = lib.types.path;
+              description = "Path to a file with environment variables to further configure the software. Currently, the only required environment variable is HCLOUD_API_TOKEN.";
+            };
+
+            privateNetworkName = lib.mkOption {
+              type = lib.types.str;
+              description = "Name of the private network on Hetzner to use to populate the DNS server.";
+            };
+
+            zoneName = lib.mkOption {
+              type = lib.types.str;
+              description = "Name of the DNS zone to update. This will be the same as the internal domain you want to use.";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.hetzner-private-dns-sync = {
+              description = "Runs hetzner-private-dns-sync once to sync server IPs and hostnames into a DNS server.";
+
+              serviceConfig = {
+                Type = "oneshot";
+                EnvironmentFile = cfg.environmentFilePath;
+                ExecStart = "${lib.getExe cfg.package} --tsig-key-path ${cfg.tsigKeyPath} --tsig-key-name ${cfg.tsigKeyName} --server-address ${cfg.serverAddress} --private-network-name ${cfg.privateNetworkName} --zone-name ${cfg.zoneName}";
+                DynamicUser = true;
+                User = "hetzner-private-dns-sync";
+                StateDirectory = "hetzner-private-dns-sync";
+                StateDirectoryMode = "0750";
+              };
+            };
+
+            systemd.timers.hetzner-private-dns-sync = {
+              description = "Timer for hetzner-private-dns-sync.";
+
+              wantedBy = [ "timers.target" ];
+
+              timerConfig = {
+                AccuracySec = "1us";
+                OnStartupSec = "10s";
+                OnUnitInactiveSec = "10s";
+              };
+            };
+          };
+        };
+
     };
 }
